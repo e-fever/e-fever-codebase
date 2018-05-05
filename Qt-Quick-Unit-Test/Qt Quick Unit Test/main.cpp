993f508586da08cb0df92a5a9fd5e0425cfa2e90
Ben Lau <xbenlau@gmail.com>
2018-05-05 23:18:09 +0800

    Export

--- Qt-Quick-Unit-Test/Qt Quick Unit Test/main.cpp
+++ Qt-Quick-Unit-Test/Qt Quick Unit Test/main.cpp
@@ -1,24 +1,7 @@
 #include <QString>
 #include <QtTest>
 #include <QtQuickTest/quicktest.h>
-
-#if defined(Q_OS_MAC) || defined(Q_OS_LINUX)
-#include <execinfo.h>
-#include <unistd.h>
-#include <signal.h>
-void handleBacktrace(int sig) {
-  void *array[100];
-  size_t size;
-
-  // get void*'s for all entries on the stack
-  size = backtrace(array, 100);
-
-  // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\\n", sig);
-  backtrace_symbols_fd(array, size, STDERR_FILENO);
-  exit(1);
-}
-#endif
+#include <XBacktrace.h>
 
 namespace AutoTestRegister {
 QUICK_TEST_MAIN(QuickTests)
@@ -26,9 +9,7 @@ QUICK_TEST_MAIN(QuickTests)
 
 int main(int argc, char *argv[])
 {
-#if defined(Q_OS_MAC) || defined(Q_OS_LINUX)
-    signal(SIGSEGV, handleBacktrace);
-#endif
+    XBacktrace::enableBacktraceLogOnUnhandledException();
 
     QGuiApplication app(argc, argv);
     return quick_test_main(argc, argv, "%{ProjectName}", TEST_SOURCE_DIR);
