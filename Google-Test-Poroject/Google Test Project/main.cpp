9e040e700d8aacc40b9338f18fe0cccd78b3a9cc
Ben Lau <xbenlau@gmail.com>
2018-09-01 16:44:18 +0800

    Add Google Test Project template

---
+++ Google-Test-Poroject/Google Test Project/main.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <math.h>
+#include <list>
+
+TEST(%{ProjectName}ests, test_basic) {
+    ASSERT_EQ(true, true);
+}
+
+int main(int argc, char** argv) {
+
+#ifdef QT_CORE_LIB
+    static_assert(True, "Qt Core is defined");
+#endif
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
